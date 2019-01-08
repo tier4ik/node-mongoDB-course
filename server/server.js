a835f1bcb11c1e79b3e804736fca18b716c837ff
Max tier4ik Shevchenko <tier4ik@gmail.com>
2019-01-08 14:37:23 +0400

    GET todos/:id

--- server/server.js
+++ server/server.js
@@ -1,15 +1,17 @@
 const express = require('express');
 // модуль с гитхаба, помогающий распарсить тело ПОСТ запроса
 const bodyParser = require('body-parser');
+const ObjectID = require('mongodb').ObjectID;
 
 var {mongoose} = require('./db/mongoose');
+
 var {Todo} = require('./models/todo');    
 var {User} = require('./models/user');    
 var {Apple} = require('./models/apples');    
 
 var app = express();
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
     var todo = new Todo({
@@ -38,6 +40,26 @@ app.get('/todos', (req, res) => {
     }, (err) => {
         res.status(400).send(err);
     });
+});
+
+// GET индивидуальные запросы с параметрами
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    if(!ObjectID.isValid(id)) {
+        return res.status(404).send('<h1>Your ID is not valid</h1>');
+    }else{
+        User.findById(id).then((user) => {
+            if(!user) {
+                return res.status(404).send('<h1>User with this ID does not exist</h1>');
+            }else{
+                res.send(`<h1>THIS IS OUR USER</h1>
+                        <div>${user}</div>
+                        <p>With the email: ${user.email}</p>`);
+            }
+        }).catch((e) => {
+            console.log(e);
+        });
+    }
 });
 
 app.listen(3000, () => {
