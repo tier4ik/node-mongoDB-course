1fa117ddfe0ec0d6156b42269d9aa34891057286
Max tier4ik Shevchenko <tier4ik@gmail.com>
2019-01-10 14:50:06 +0400

    Add password-hashing script with bcrypt library

--- server/models/user.js
+++ server/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
 const _ = require('lodash');
+const bcrypt = require('bcryptjs');
 
 var Schema = mongoose.Schema;
 var UserSchema = new Schema({
@@ -68,6 +69,19 @@ UserSchema.static('findByToken', function(token) {
         'tokens.token': token,
         'tokens.access': 'auth'
     });
+});
+
+UserSchema.pre('save', function(next) {
+    if(this.isModified('password')) {
+        bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(this.password, salt, (err, hash) => {
+                this.password = hash;
+                 next();
+            });
+        });
+    }else{
+        next();
+    }
 });
 
 var User = mongoose.model('User', UserSchema);
